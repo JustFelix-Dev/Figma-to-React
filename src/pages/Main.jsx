6bc67ef554fa3c54557a76504cf135eb3d1b799f
Felix Owolabi <owolabifelix78@gmail.com>
2023-06-16 09:36:13 +0100

    Figma-to-Code

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -1,6 +1,12 @@
+import { useState } from "react";
 import { Outlet } from "react-router-dom";
 
 const Main = () => {
+    const [ open, setOpen ] = useState(false)
+
+    const handleClick = ()=>{
+        setOpen(true)
+    }
     return ( 
             <>
               <nav>
@@ -8,14 +14,14 @@ const Main = () => {
                     <span>Minimal.</span>
                 </div>
                 <div className="links">
-                    <ul>
+                    <ul className={ open ? 'active':""}>
                         <li className="active"><a href="#">Shop</a></li>
                         <li><a href="#">About</a></li>
                         <li><a href="#">Contact</a></li>
                         <li><a href="#">FAQ</a></li>
                     </ul>
                     <div className="cart">
-                        <img src="images/menu.png" alt="hamburger-menu" />
+                        <img onClick={handleClick} src="images/menu.png" alt="hamburger-menu" />
                         <img src="images/shopping-bag.png" alt="cart" />
                     </div>
                 </div>
