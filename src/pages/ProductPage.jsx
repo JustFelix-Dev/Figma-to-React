7143ad36a036501227bbcb4d1913241f91fffbe2
Felix Owolabi <owolabifelix78@gmail.com>
2023-06-16 14:48:58 +0100

    Figma-to-Code

--- src/pages/ProductPage.jsx
+++ src/pages/ProductPage.jsx
@@ -18,8 +18,36 @@ const ProductPage = () => {
         </div>
 
         <div className="Texts">
+            <p className='first'>Lorem Ipsum es</p>
+            <p className='second'>Lorem Ipsum es sim</p>
+            <p className='third'>
+            Lorem Ipsum es simplemente el texto de relleno de las imprentas y archiv  de texto. Lorem Ipsum ha sido el</p>
+            <div className="modes">
+                <span><img src="images/dark-mode.png" alt="darkmode-btn" /></span>
+                <span><img src="images/light-mode.png" alt="lightmode-btn" /></span>
+            </div>
+            <div className="measurements">
+                <div className="aspect-ratio">
+                    <div className="height"></div>
+                    <div className="width"></div>
+                </div>
+                <div className="size">
+                    <span>Size in Inches</span>
+                </div>
+            </div>
+            HEIGHT
+            WIDTH
+            Size in Inches
+            52 cm
+            43 cm
+            $69.00
+            1
+            ADD TO CART
+            BUY NOW
         </div>
+      
        </div>
+       
        </>
   )
 }
